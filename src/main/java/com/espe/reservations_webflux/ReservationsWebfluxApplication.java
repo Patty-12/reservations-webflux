61841ced88fc9c1ece889777345207b59f0344c5
Patty-12 <mpatricia.lapuerta@gmail.com>
2026-07-09 17:44:48 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/com/espe/reservations_webflux/ReservationsWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.espe.reservations_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espe/reservations_webflux/ReservationsWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espe.reservations_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
